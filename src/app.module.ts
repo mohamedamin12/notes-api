949bfc4469c94adb2e8c70ba887e65c9afe13bc0
mohamedamin12 <aboamin909080@gmail.com>
2025-03-19 21:17:39 +0200

    add initial entities and DTOs for auth, notes, users, and folders; remove unused app service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { FoldersModule } from './folders/folders.module';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    AuthModule, UsersModule, FoldersModule, NotesModule, UsersModule
+  ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import * as compression from 'compression';
+import rateLimit from 'express-rate-limit';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(helmet());
+
+  app.use(compression());
+
+  app.enableCors({
+    origin: process.env.ALLOWED_ORIGINS?.split(',') || '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, 
+      max: 100,
+      message: 'Too many requests from this IP, please try again later.',
+    }),
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(5000);
 }
 bootstrap();
+
