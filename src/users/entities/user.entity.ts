e00581f7d3750a26b53ff6281090f47fbf155ce9
mohamedamin12 <aboamin909080@gmail.com>
2025-03-19 21:24:16 +0200

    feat(users): implement user entity with relationships and basic service methods

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Folder } from "src/folders/entities/folder.entity";
+import { Note } from "src/notes/entities/note.entity";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+    @Column({ unique: true, nullable: false, length: 30 })
+    username: string;
+    @Column({ unique: true, nullable: false, length: 100 })
+    email: string;
+    @Column({ nullable: false })
+    password: string;
+    @OneToMany(() => Folder, (folder) => folder.user)
+    folders: Folder[];
+    @OneToMany(() => Note, (note) => note.user)
+    notes: Note[];
+    @CreateDateColumn()
+    createdAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async findByUsernameOrEmail(identifier: string): Promise<User | null> {
+    return this.usersRepository.findOne({
+      where: [{ username: identifier }, { email: identifier }],
+    });
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async createUser(username: string, email: string, password: string): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = this.usersRepository.create({ username, email, password: hashedPassword });
+    return this.usersRepository.save(user);
   }
 }
